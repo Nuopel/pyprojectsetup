3e4b4d922adf2c927a5b3a6de54a09ee8ed7738c
Samuel <samuel.dupont@arteac-lab.fr>
2024-02-12 17:45:40 +0100

    maj: exclude additional_exclude_packages

--- examples/Example_look_packages_into_project.py
+++ examples/Example_look_packages_into_project.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     filepath  = '../' # filepath to look
     flag_verbose = False # make it extra talkative about which file import which package
     excluded_folders = ['venv', 'build'] # folder that don't need to be looked for packages
-
-    packages = get_unique_packages_from_filepath(filepath=filepath, excluded_folders=excluded_folders, flag_verbose=flag_verbose)
+    additional_exclude_packages = [] # additional packages that you dont want to add (native python one for exemple)
+    packages = get_unique_packages_from_filepath(filepath=filepath, excluded_folders=excluded_folders, flag_verbose=flag_verbose, additional_exclude_packages=additional_exclude_packages)
     pypi_packages, localpy_packages, undetermined_packages = categorize_packages(packages)
 
     print("Looking into  :", os.path.abspath(filepath))
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     print("PyPI Packages to put into requirements.txt : \n")
     for item in pypi_packages:
         print(item)
-        
+
 
     print("Updating requirements.txt : \n")
     update_requirements_txt(pypi_packages, './requirements.txt')
